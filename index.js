3437bd0bdbd2e94f0968f6b2e7b0d58334011978
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2022-06-16 21:55:19 -0400

    commit tree 474925f6bd60f0c14743f548a797495d7b988e80

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+    console.log (`I'm ${age} years old.Happy birthday to me!`);
+    
+}
+
+const gifts = ["teddy bear","drone","doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i<gifts.lenght; i++){
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger
+}
+return gifts;
+}
+wrapGifts(gifts);
+*/
+const cards = ["Charlie", "Samip", "Ali" ];
+
+function writeCards(cards){
+    for (let i = 0; i < cards.lenght; i++){
+    console.log(`Thank you,${cards[i]}, for the wonderful birthday gift!`)
+    }
+    return cards;
+}
